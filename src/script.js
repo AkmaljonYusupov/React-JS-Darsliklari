925fd4f400b4159a8e16d94ac756a7b9db9cb1fa
AkmaljonYusupov <InfoAkmaljon.uz@gmail.com>
2022-09-21 19:47:55 +0500

    dars-4 end

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const obj = {
+  name: "Johan",
+  surname: "Johanov",
+  status: "programmer",
+};
+
+const name = "surname";
+
+console.log(obj[name]);
+console.log(name);
+
+const data = [{ name: "html" }, { name: "css" }, { name: "js" }];
